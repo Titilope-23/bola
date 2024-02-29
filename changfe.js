9e6b8a6658cbdfdb245bebb3f5fa36aa13724c55
Lateefat Omotoso <omotosolateefat23@gmail.com>
2024-02-29 11:01:02 -0800

    Javascript

--- changfe.js
+++ changfe.js
@@ -1,1 +1,16 @@
- 
+function checkScore(){
+    console.log(input.value);
+   
+    if(input.value < 40){
+        alert("You failed F")
+    } else if(input.value >= 70){
+        alert("Excellent A")
+    } else if(input.value <= 50 ){
+        alert("Try harder D")
+    } else if(input.value <=60){
+        alert("Good C")
+    } else if(input.value >=61){
+        alert('Very Good B')
+    }
+
+}
