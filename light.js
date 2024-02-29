57021f58a289dbb42906183b195b8a19aee358ee
Lateefat Omotoso <omotosolateefat23@gmail.com>
2024-02-29 10:06:42 -0800

    Javascript

--- light.js
+++ light.js
@@ -10,3 +10,17 @@ function offBulb(){
     document.getElementById("text-1").innerHTML = "Let's OFF the Bulb"
  
 }
+
+
+function offfBulb(){
+    document.getElementById("imgg").src="img/pic_bulboff.gif"
+    document.getElementById("text-2").style.color = "green"
+    document.getElementById("text-2").innerHTML = "Let's OFF the Bulb"
+}
+
+function onnBulb(){
+    document.getElementById("imgg").src ="img/pic_bulbon.gif"
+    document.getElementById("text-2").style.color = "brown"
+    document.getElementById("text-2").innerHTML = "Let's ON the Bulb"
+ 
+}
